a2ae79a7619b3dff8479fdd33e8dc90896bf192c
lishengbing <1269113083@qq.com>
2016-12-09 13:43:11 +0800

    数据

--- XJDomainLive/XJDomainLive/Classes/Home/Controller/List/XJLiveListViewController.swift
+++ XJDomainLive/XJDomainLive/Classes/Home/Controller/List/XJLiveListViewController.swift
@@ -18,7 +18,6 @@ class XJLiveListViewController: UIViewController {
         setupUI()
         loadHomeData()
     }
-
 }
 
 
@@ -53,9 +52,14 @@ extension XJLiveListViewController : UITableViewDelegate, UITableViewDataSource 
         return cell
     }
     
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85 + UIScreen.main.bounds.width
+    }
+    
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.layer.transform = CATransform3DMakeScale(0.3, 0.3, 1)
-        UIView.animate(withDuration: 0.8, delay: 0.0, options: [], animations: {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [], animations: {
             cell.layer.transform = CATransform3DMakeScale(1, 1, 1)
         }, completion: { (_) in
             let anim = CATransition()
